658120deb4ca18d04983d50c123a79224cae75f5
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-04-30 16:13:50 +0530

    Create model Designation

---
+++ E-Medy/src/emedy/model/Designation.java
@@ -0,0 +1,33 @@
+package emedy.model;
+
+public class Designation {
+    int designation_id;
+    String name;
+    double wage;
+
+    public int getDesignation_id() {
+        return designation_id;
+    }
+
+    public void setDesignation_id(int designation_id) {
+        this.designation_id = designation_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWage() {
+        return wage;
+    }
+
+    public void setWage(double wage) {
+        this.wage = wage;
+    }
+    
+    
+}
