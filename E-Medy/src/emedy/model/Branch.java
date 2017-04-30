0e07c93507b5193867d8f0e45ce393520a651546
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-04-30 16:19:56 +0530

    Create model Branch

---
+++ E-Medy/src/emedy/model/Branch.java
@@ -0,0 +1,23 @@
+package emedy.model;
+
+public class Branch {
+    int branch_id;
+    String name;
+
+    public int getBranch_id() {
+        return branch_id;
+    }
+
+    public void setBranch_id(int branch_id) {
+        this.branch_id = branch_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
