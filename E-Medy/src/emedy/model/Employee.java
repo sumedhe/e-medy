438b695f683043c4b9c6ba00df20c423f9ea14b6
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-04-30 15:47:52 +0530

    Create model Employee

---
+++ E-Medy/src/emedy/model/Employee.java
@@ -0,0 +1,107 @@
+package emedy.model;
+
+import java.util.Date;
+
+public class Employee {
+    int employee_id;
+    String first_name;
+    String last_name;
+    String nic;
+    Date dob;
+    char Gender;
+    String address;
+    String phone;
+    String mobile;
+    Date start_date;
+    int designation_id;
+
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public char getGender() {
+        return Gender;
+    }
+
+    public void setGender(char Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public int getDesignation_id() {
+        return designation_id;
+    }
+
+    public void setDesignation_id(int designation_id) {
+        this.designation_id = designation_id;
+    }
+    
+    
+}
